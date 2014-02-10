699bbaaecd7b51a95a65fae9fd669ab320c8249d
dhuang <dhuang.cn@gmail.com>
2014-02-10 19:46:33 +0800

    distinguish the concepts between 'term' and 'token'

--- Dict.h
+++ Dict.h
@@ -1,15 +1,19 @@
 #ifndef _DICT_H_
 #define _DICT_H_
 
+#include "Dict.h"
 #include <unordered_map>
 #include <string>
 
 using namespace std;
 
 class AbstractDict {
+protected:
+	string filePath;
 public:
-	virtual void add(const string &token) = 0;
-	virtual size_t getTokenID(const string &token) const = 0;
+	AbstractDict(const string &filePath) : filePath(filePath) {}
+	virtual void add(const string &term) = 0;
+	virtual size_t getTermID(const string &term) const = 0;
 	virtual ~AbstractDict() {}
 };
 
@@ -17,17 +21,18 @@ class NormalDict : public AbstractDict {
 private:
 	unordered_map<string, int> dict;
 public:
-	void add(const string &token);
-	size_t getTokenID(const string &token) const;
+	NormalDict(const string &filePath, bool isNew=false);
+	void add(const string &term);
+	size_t getTermID(const string &term) const;
+	~NormalDict();
+	friend ostream &operator << (ostream &out, const NormalDict &dict);
+	friend istream &operator >> (istream &in, NormalDict &dict);
 };
 
 class HugeDict : public AbstractDict {
-private:
-	string filePath;
 public:
-	HugeDict(const string &filePath);
-	void add(const string &token);
-	size_t getTokenID(const string &token) const;
+	void add(const string &term);
+	size_t gettermID(const string &term) const;
 };
 
 #endif
--- NormalDict.cpp
+++ NormalDict.cpp
@@ -1,15 +1,41 @@
 #include "Dict.h"
+#include <fstream>
 #include <unordered_map>
 
 using namespace std;
 
-void NormalDict::add(const string &token) {
-	auto it = dict.find(token);
-	if ( it == dict.end() ) dict[token] = dict.size() + 1;
+
+NormalDict::NormalDict(const string &filePath, bool isNew)
+	: AbstractDict(filePath) {
+	if ( !isNew ) {
+		ifstream in(filePath);
+		in >> *this;
+		in.close();
+	}
 }
 
-size_t NormalDict::getTokenID(const string &token) const {
-	auto it = dict.find(token);
+void NormalDict::add(const string &term) {
+	auto it = dict.find(term);
+	if ( it == dict.end() ) dict[term] = dict.size() + 1;
+}
+
+size_t NormalDict::getTermID(const string &term) const {
+	auto it = dict.find(term);
 	return it == dict.end() ? 0 : it->second;
+}
+
+NormalDict::~NormalDict() {
+	ofstream out(filePath);
+	out << *this;
+	out.close();
+}
+
+
+istream &operator >> (istream &in, NormalDict &dict) {
+	return in;
+}
+
+ostream &operator << (ostream &out, const NormalDict &dict) {
+	return out;
 }
 
