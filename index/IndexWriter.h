403b9dbc8d39ef9ea65569fa83984d1831e2107c
Da-Huang <dhuang.cn@gmail.com>
2014-02-28 20:17:27 +0800

    Add Interface to IndexWriter.

--- index/IndexWriter.h
+++ index/IndexWriter.h
@@ -1,8 +1,22 @@
 #ifndef _INDEX_WRITER_H_
 #define _INDEX_WRITER_H_
 
+#include "Document.h"
+
 class IndexWriter {
+private:
+	FILE *dir;
 public:
+	void write(Document &doc);
+	void close();
 };
+
+void IndexWriter::write(Document &doc) {
+}
+
+
+void IndexWriter::close() {
+}
+
 
 #endif
