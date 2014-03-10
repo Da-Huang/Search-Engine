eed11755021b6783862586215e3affa55e3d1106
Da-Huang <dhuang.cn@gmail.com>
2014-03-10 13:44:45 +0800

    skip position list.

--- index/posting/PostingStream.h
+++ index/posting/PostingStream.h
@@ -20,6 +20,9 @@ private:
 public:
 	PostingStream(istream &in, size_t begin, size_t end);
 	virtual Posting next();
+	virtual Posting peek();
+	virtual size_t nextDocID();
+	virtual size_t peekDocID();
 	virtual inline bool hasNext() const { return current < end; }
 	virtual ~PostingStream() {}
 };
